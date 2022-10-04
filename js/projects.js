0d4235df629ad1fbef60be6d5cac7c87966f658b
nagamaninaga <111356935+nagamaninaga@users.noreply.github.com>
2022-10-04 21:07:24 +0530

    Add files via upload

---
+++ js/projects.js
@@ -0,0 +1,108 @@
+const projects = [{
+        img: "./assets/img/rentify.jpg",
+        name: "Rentify",
+        category: "fullstack",
+        techstack: ['html5', 'css-3', 'mongo', 'react-2', 'nodejs'],
+        frontend: "https://github.com/nagamaninaga/Rentify",
+        backend: "https://github.com/nagamaninaga/RentifyBackend",
+        deploymentUrl: "https://zen-rentify.netlify.app/",
+        description: `A single page application for renting vehicles with razorpay integration for payments.`
+    },
+    {
+        img: "./assets/img/memories.jpg",
+        name: "Memories",
+        category: "fullstack",
+        techstack: ['html5', 'css-3', 'mongo', 'react-2', 'nodejs', 'firebase'],
+        frontend: "https://github.com/nagamaninaga/Memories-frontend",
+        backend: "https://github.com/nagamaninaga/Memories-backend",
+        deploymentUrl: "https://zen-memories.netlify.app/",
+        description: `A web application similar to instagram where users can post media and chat with other users`
+    },
+    {
+        img: "./assets/img/ticket.jpg",
+        name: "Ticket Management System",
+        category: "fullstack",
+        techstack: ['html5', 'css-3', 'mongo', 'react-2', 'nodejs'],
+        frontend: "https://github.com/nagamaninaga/TicketManagementTool",
+        backend: "https://github.com/nagamaninaga4/TMTBackend",
+        deploymentUrl: "https://zen-ticketmanagement.netlify.app/",
+        description: `A single page application on ticket management system built using MERN stack`
+    },
+    {
+        img: "./assets/img/bookmyhall.png",
+        name: "Book My Hall",
+        category: "backend",
+        techstack: ['html5', 'css-3', 'bootstrap-4', 'mongo', 'nodejs'],
+        frontend: "https://github.com/nagamaninaga/bookmyhall-frontend",
+        backend: "https://github.com/nagamaninaga/BookMyHall",
+        deploymentUrl: "https://bookmyhall-zen.netlify.app/",
+        description: `A Hall booking system developed the backend with the help of Node.JS and Express.Js`
+    },
+    {
+        img: "./assets/img/urlshortener.png",
+        name: "URL Shortener",
+        category: "backend",
+        techstack: ['html5', 'css-3', 'bootstrap-4', 'mongo', 'nodejs'],
+        frontend: "https://github.com/nagamaninaga/URLShortener-frontend",
+        backend: "https://github.com/nagamaninaga/URLShortener-backend",
+        deploymentUrl: "https://zen-urlshortener.netlify.app/",
+        description: `A simple Web application for shortening the URLs, developed using Node.Js and Express.Js`
+    },
+    {
+        img: "./assets/img/mentors.jpg",
+        name: "My Mentors",
+        category: "backend",
+        techstack: ['html5', 'css-3', 'mongo', 'nodejs'],
+        backend: "https://github.com/nagamaninaga/Mentors",
+        deploymentUrl: "https://zen-mentors.herokuapp.com/",
+        description: `A basic web application supporting all CRUD operations to map students to mentors.`
+    },
+    {
+        img: "./assets/img/game.jpg",
+        name: "PIG Game",
+        category: "frontend",
+        techstack: ['html5', 'css-3', 'javascript-1'],
+        frontend: "https://github.com/nagamaninaga/Guvi-Tasks/tree/master/PIG%20game",
+        deploymentUrl: "https://nagamaninaga.github.io/Guvi-Tasks/PIG%20game/",
+        description: `A fun and simple two player game where players rolls dice to get a winning score`
+    },
+    {
+        img: "./assets/img/quiz.jpg",
+        name: "General Trivia",
+        category: "frontend",
+        techstack: ['html5', 'css-3', 'javascript-1'],
+        frontend: "https://github.com/nagamaninaga/Trivia",
+        deploymentUrl: "https://zen-trivia.netlify.app/",
+        description: `A web app for random category general quiz developed using opentb api`
+    },
+    {
+        img: "./assets/img/cricket.jpg",
+        name: "Cricket Game",
+        category: "frontend",
+        techstack: ['html5', 'css-3', 'javascript-1', 'bootstrap-4'],
+        frontend: "https://github.com/nagamaninaga/Guvi-Tasks/tree/master/Cricket",
+        deploymentUrl: "https://cricket-zentask.netlify.app/",
+        description: `Simple cricket game that announces winner and man of the match based on team score `
+    },
+    {
+        img: "./assets/img/nyt.jpg",
+        name: "New York Times",
+        category: "frontend",
+        techstack: ['html5', 'css-3', 'javascript-1', 'bootstrap-4'],
+        frontend: "https://nagamaninaga.github.io/Guvi-Tasks/Assessment4/",
+        deploymentUrl: "https://newyorktimes-assessment4.netlify.app/",
+        description: `A simple newspaper application built with the help of new york times developer api `
+    },
+    {
+        img: "./assets/img/card.jpg",
+        name: "Description card",
+        category: "frontend",
+        techstack: ['html5', 'css-3', 'javascript-1'],
+        frontend: "https://github.com/nagamaninaga/Guvi-Tasks/tree/master/Transform",
+        deploymentUrl: "https://nagamaninaga.github.io/Guvi-Tasks/Transform/",
+        description: `A stylish single page card that can be modeled to a greeting or description card`
+    }
+
+]
+
+//module.exports.projects = projects;
